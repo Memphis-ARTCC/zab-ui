a3755e10dadfd3dd2546835efd976ef0e76faf18
Jacob Boyles <fixterjake@gmail.com>
2025-05-26 21:05:16 -0400

    fix training record

--- src/views/instructor/training/UserSessions.vue
+++ src/views/instructor/training/UserSessions.vue
@@ -24,7 +24,7 @@
 					<tr v-for="session in sessions" :key="session._id">
 						<td>{{ dtLong(session.startTime) }}</td>
 						<td>{{ dtLong(session.endTime) }}</td>
-						<td>{{ session.milestone.name }}</td>
+						<td>{{ session.milestone?.name ?? "N/A" }}</td>
 						<td>{{ session.instructor ? `${session.instructor.fname} ${session.instructor.lname}` : session.instructorCid }}</td>
 						<td class="options">
 							<router-link :to="`/ins/training/session/${session._id}`" data-position="top" data-tooltip="View Details" class="tooltipped">
